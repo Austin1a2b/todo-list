62133b79358fb172b9681075294dd6596c15d6f3
Austin1a2b <cavalier283@gmail.com>
2021-12-04 13:30:36 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list') // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.get('/', (req, res) => {
   res.send('hi~')

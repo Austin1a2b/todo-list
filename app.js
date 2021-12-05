15dab7c0f95648a63b9437a75c9bd0c30c5100c2
Austin1a2b <cavalier283@gmail.com>
2021-12-05 11:48:36 +0800

    瀏覽特定Todo

--- app.js
+++ app.js
@@ -46,6 +46,13 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/todos/:id', (req, res) => {
+  const =req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('detail', { todo: todo }))
+    .catch(error => console.log(error))
+})
 
 
 
